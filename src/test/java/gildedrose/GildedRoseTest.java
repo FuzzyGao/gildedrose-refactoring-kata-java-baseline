4f1b729f78f1cbf93a8e521ce1e57b9bc9754132
gaoting01 <111111>
2020-07-22 14:39:58 +0800

    homework

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -1,17 +1,21 @@
 package gildedrose;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+
+    public void updateQuality() {
+        CombinationApprovals.verifyAllCombinations(
+                this::doUpdateQuality,
+                new String[]{"foo","Aged Brie","Backstage passes to a TAFKAL80ETC concert","Sulfuras, Hand of Ragnaros"},
+                new Integer[]{-1,0,1,4,5,6,9,10,11},
+                new Integer[]{0,1,2,3,46,47,48,49,50}
+        );
     }
 
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        return app.items[0].toString();
+    }
 }
--- src/test/java/gildedrose/TexttestFixture.java
+++ src/test/java/gildedrose/TexttestFixture.java
@@ -1,18 +1,20 @@
 package gildedrose;
 
+import gildedrose.Item;
+
 public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
         Item[] items = new Item[]{
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("+5 Dexterity Vest", 10, 0), //
-                new Item("+5 Dexterity Vest", 0, 3), //
-                new Item("+5 Dexterity Vest", 0, 1), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Aged Brie", 2, 50), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("+5 Dexterity Vest", 10, 0),
+                new Item("+5 Dexterity Vest", 0, 3),
+                new Item("+5 Dexterity Vest", 0, 1),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Aged Brie", 2, 0),
+                new Item("Aged Brie", 2, 50),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 50),
@@ -36,7 +38,7 @@ public class TexttestFixture {
         }
 
         for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
+            System.out.println("---- day " + i + " ----");
             System.out.println("name, sellIn, quality");
             for (Item item : items) {
                 System.out.println(item);
